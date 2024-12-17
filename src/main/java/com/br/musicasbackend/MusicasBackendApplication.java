8311982830764710dd337f4446184300590ee5f1
jeanheberth <jeanheberth19@gmail.com>
2024-12-17 12:04:53 -0300

    Initial commit

---
+++ src/main/java/com/br/musicasbackend/MusicasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.musicasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicasBackendApplication.class, args);
+    }
+
+}
